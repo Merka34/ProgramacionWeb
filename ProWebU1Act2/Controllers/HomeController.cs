a36a7d9a8b20529cd14ad51f65a79e22b04ae4d2
Merka34 <videoscreepyloq@gmail.com>
2021-09-24 14:43:13 -0500

    Agregar archivos de proyecto.

---
+++ ProWebU1Act2/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProWebU1Act2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProWebU1Act2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(IndexViewModel vm)
+        {
+            return View(vm);
+        }
+    }
+}
---
+++ ProWebU1Act2/Models/IndexViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProWebU1Act2.Models
+{
+    public class IndexViewModel
+    {
+        public int Calificacion1 { get; set; }
+        public int Calificacion2 { get; set; }
+        public int Calificacion3 { get; set; }
+        public int CalificacionFinal { get { return (Calificacion1 + Calificacion2 + Calificacion3) / 3; } }
+
+        public string Estado
+        {
+            get
+            {
+                if (CalificacionFinal >= 70)
+                    return "Aprobado";
+                else
+                    return "Reprobado";
+            }
+        }
+    }
+}
